d854990cf133bd4b9df75f6e01ccc72c6efb0980
ibilalchaudhary <bilalchaudhary478@gmail.com>
2021-12-22 15:04:30 +0500

    Update PhoneInput.js

--- PhoneInput.js
+++ PhoneInput.js
@@ -14,6 +14,26 @@ export default function PhoneInput() {
         placeholder="Phone Number"
         label="Enter phone number"
         value={Phone}
+         internationalIcon={() => {
+              return (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="22"
+                  height="22"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="1"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-globe"
+                >
+                  <circle cx="12" cy="12" r="10"></circle>
+                  <line x1="2" y1="12" x2="22" y2="12"></line>
+                  <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
+                </svg>
+              );
+            }}
         // onChange={(e) => setPhone( e.currentTarget.value)}
         onChange={setPhone}
       />
